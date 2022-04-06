7131563ba2d0d4a37db535356a2b75de094963f6
Andy Lo <chunyulo@arch.nycu.edu.tw>
2022-04-06 16:58:39 +0800

    Handle cors issue

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const importData = require("./data.json");
 let port = process.env.PORT || 3000;
 const devicesRoutes = require("./routes/devices");
@@ -8,6 +9,7 @@ const devicesRoutes = require("./routes/devices");
 // middleware
 app.use(bodyParser.json());
 app.use("/devices", devicesRoutes);
+app.use(cors({ origin: "*" }));
 
 app.get("/", (req, res) => {
   res.send("This is Homepage");
